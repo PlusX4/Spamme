62b628602ff0d109d4deb8a1fa1af8bc910cdb5f
Plus-X4 <47147208+PlusX4@users.noreply.github.com>
2019-02-09 00:42:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 pkg install php -y
 pkg install pyhton2 -y
+pkg install unzip -y
 cd Modules
+unzip 1.zip
+rm -rif *zip
 chmod 777 * 
 cd .Spammer-Grab
 chmod +x *
 ./install.sh
 cd ../..
-chmod +x .sh
+chmod +x *.sh
 ls
